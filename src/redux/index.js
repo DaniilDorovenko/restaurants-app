237f2d0341531a94250110fcc8882521d925d678
DaniilDorovenko <dorovenco@mail.ru>
2024-02-15 10:53:34 +0300

    get queres for RTK added

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,20 +1,25 @@
-import { configureStore, combineSlices  } from "@reduxjs/toolkit";
+import { configureStore, combineSlices } from "@reduxjs/toolkit";
 import { restaurantSlice } from "./entities/restaurant";
 import { reviewSlice } from "./entities/review";
 import { userSlice } from "./entities/user";
 import { dishSlice } from "./entities/dish";
 import { requestSlice } from "./ui/requset";
 import { cartSlice } from "./ui/cart";
+import { api } from "./services/api";
 
 export const store = configureStore({
     reducer: combineSlices(
-        restaurantSlice, 
-        reviewSlice, 
-        userSlice, 
+        restaurantSlice,
+        reviewSlice,
+        userSlice,
         dishSlice,
         requestSlice,
         cartSlice,
-        ),
+        api
+    ),
+
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(api.middleware),
 });
 
 // console.log(store.getState());
---
+++ src/redux/services/api.js
@@ -0,0 +1,37 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+export const api = createApi({
+    reducerPath: 'api',
+    tagTypes: ["Dish", "Restaurant", "Review", "User"],
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001/api/" }),
+    endpoints: (builder) => ({
+        getDishes: builder.query({
+            query: () => ({
+                url: "dishes",
+            }),
+        }),
+        getRestaurants: builder.query({
+            query: () => ({
+                url: "restaurants",
+            }),
+        }),
+        getReviews: builder.query({
+            query: () => ({
+                url: "reviews"
+            }),
+        }),
+        getUsers: builder.query({
+            query: () => ({
+                url: "users",
+            }),
+        }),
+    })
+})
+
+export const {
+    useGetDishesQuery,
+    useGetRestaurantsQuery,
+    useGetReviewsQuery,
+    useGetUsersQuery,
+} = api;
